0b555759ed5d7aa9e3ef8c72d9d16a2061a70cdc
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2024-03-27 22:05:51 -0400

    Core: Lower timeout and add better logging for timeouts

--- Action-Delay-API-Core/Broker/CloudflareAPIBroker.cs
+++ Action-Delay-API-Core/Broker/CloudflareAPIBroker.cs
@@ -16,6 +16,7 @@ public partial class CloudflareAPIBroker : ICloudflareAPIBroker
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri("https://api.cloudflare.com");
             _httpClient.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher;
+            _httpClient.Timeout = TimeSpan.FromSeconds(30);
         }
 
 
--- Action-Delay-API-Core/Extensions/HttpExtensions.cs
+++ Action-Delay-API-Core/Extensions/HttpExtensions.cs
@@ -24,7 +24,9 @@ namespace Action_Delay_API_Core.Extensions
                 {
                     logger.LogCritical(
                         $"Could not get response {assetName} from API, API returned nothing, Status Code: {httpResponse.StatusCode}");
-                    return Result.Fail(new CustomAPIError($"Could not get response {assetName} from API, API returned nothing, Status Code: {httpResponse.StatusCode}", (int)httpResponse.StatusCode, "API Empty Response", "", listener.GetTime()));
+                    return Result.Fail(new CustomAPIError(
+                        $"Could not get response {assetName} from API, API returned nothing, Status Code: {httpResponse.StatusCode}",
+                        (int)httpResponse.StatusCode, "API Empty Response", "", listener.GetTime()));
                 }
 
                 ApiResponse<TResult>? response = null;
@@ -35,14 +37,22 @@ namespace Action_Delay_API_Core.Extensions
                 catch (Exception ex)
                 {
                     // Better messages for Deserialization errors
-                    logger.LogCritical(ex, "Failed to Deserialize: {ex} Response: {rawString}", ex.Message, rawString.Truncate(25));
-                    return Result.Fail(new CustomAPIError($"Issue reading response, Status Code: {httpResponse.StatusCode}: {httpResponse.ReasonPhrase}, Response: {rawString.Truncate(50)}", (int)httpResponse.StatusCode, $"Failure parsing response: {rawString.Truncate(25)}", "", listener.GetTime()));
+                    logger.LogCritical(ex, "Failed to Deserialize: {ex} Response: {rawString}", ex.Message,
+                        rawString.Truncate(25));
+                    return Result.Fail(new CustomAPIError(
+                        $"Issue reading response, Status Code: {httpResponse.StatusCode}: {httpResponse.ReasonPhrase}, Response: {rawString.Truncate(50)}",
+                        (int)httpResponse.StatusCode, $"Failure parsing response: {rawString.Truncate(25)}", "",
+                        listener.GetTime()));
                 }
 
                 if (response == null)
                 {
-                    logger.LogCritical($"Could not get response {assetName} from API, status code: {httpResponse.StatusCode}");
-                    return Result.Fail((new CustomAPIError($"Could not get response {assetName} from API, status code: {httpResponse.StatusCode} Response: {rawString.Truncate(50)}", (int)httpResponse.StatusCode, $"Could not read deseralized response: {rawString.Truncate(25)}", "", listener.GetTime())));
+                    logger.LogCritical(
+                        $"Could not get response {assetName} from API, status code: {httpResponse.StatusCode}");
+                    return Result.Fail((new CustomAPIError(
+                        $"Could not get response {assetName} from API, status code: {httpResponse.StatusCode} Response: {rawString.Truncate(50)}",
+                        (int)httpResponse.StatusCode, $"Could not read deseralized response: {rawString.Truncate(25)}",
+                        "", listener.GetTime())));
                 }
 
                 if (response.Errors != null && response.Errors.Any())
@@ -52,7 +62,12 @@ namespace Action_Delay_API_Core.Extensions
                         logger.LogCritical($"Error with {assetName}: {error}");
                     }
 
-                    return Result.Fail(new CustomAPIError($"Error with {assetName}: {String.Join(" | ", response.Errors.Select(error => $"{error.Code} - {error.Message}"))}, status code: {httpResponse.StatusCode}", (int)httpResponse.StatusCode, $"Error: {String.Join(" | ", response.Errors.Select(error => $"{error.Code} - {error.Message}"))}", response.Errors?.FirstOrDefault(error => error.Code != null)?.Code.ToString() ?? "", listener.GetTime()));
+                    return Result.Fail(new CustomAPIError(
+                        $"Error with {assetName}: {String.Join(" | ", response.Errors.Select(error => $"{error.Code} - {error.Message}"))}, status code: {httpResponse.StatusCode}",
+                        (int)httpResponse.StatusCode,
+                        $"Error: {String.Join(" | ", response.Errors.Select(error => $"{error.Code} - {error.Message}"))}",
+                        response.Errors?.FirstOrDefault(error => error.Code != null)?.Code.ToString() ?? "",
+                        listener.GetTime()));
                 }
 
                 if (response.Messages != null && response.Messages.Any())
@@ -65,22 +80,32 @@ namespace Action_Delay_API_Core.Extensions
 
                 if (response.Success == false)
                 {
-                    logger.LogCritical($"Response Success did not indicate success, status code: {httpResponse.StatusCode}");
-                    return Result.Fail(new CustomAPIError($"Response Success did not indicate success but returned no errors, status code: {httpResponse.StatusCode}", (int)httpResponse.StatusCode, $"Non-Success with no errors, response body: {rawString}", "", listener.GetTime()));
+                    logger.LogCritical(
+                        $"Response Success did not indicate success, status code: {httpResponse.StatusCode}");
+                    return Result.Fail(new CustomAPIError(
+                        $"Response Success did not indicate success but returned no errors, status code: {httpResponse.StatusCode}",
+                        (int)httpResponse.StatusCode, $"Non-Success with no errors, response body: {rawString}", "",
+                        listener.GetTime()));
                 }
+
                 response.ResponseTimeMs = listener.GetTime();
                 return response;
             }
+    
             catch (HttpRequestException ex)
             {
                 logger.LogCritical(ex, $"Unexpected HTTP Error: API Returned: {httpResponse?.StatusCode} - {ex.Message}");
                 return Result.Fail(new CustomAPIError($"Unexpected HTTP Error: API Returned: {httpResponse?.StatusCode} - {ex.Message}", (int)(httpResponse?.StatusCode ?? 0) , $"API Error, reason: {ex.Message}", "", listener.GetTime()));
             }
+            catch (OperationCanceledException ex)
+            {
+                logger.LogCritical(ex, $"Unexpected Timeout Error: {ex.Message}");
+                return Result.Fail(new CustomAPIError($"Unexpected Timeout Error: {ex.Message}", (int)(httpResponse?.StatusCode ?? 0), $"API Error, reason: {ex.Message}", "", listener.GetTime()));
+            }
             catch (Exception ex)
             {
                 logger.LogCritical(ex, $"Unexpected Error: API Returned: {httpResponse?.StatusCode}");
                 return Result.Fail(new CustomAPIError($"Unexpected Error: API Returned: {httpResponse?.StatusCode}", (int)(httpResponse?.StatusCode ?? 0), $"Unknown API Error", "", listener.GetTime()));
-
             }
 
             return null;
@@ -162,6 +187,11 @@ this HttpClient client, HttpRequestMessage httpRequest, string assetName, ILogge
             {
                 logger.LogCritical(ex, $"Unexpected HTTP Error: API Returned: {httpResponse?.StatusCode} - {ex.Message}");
                 return Result.Fail(new CustomAPIError($"Unexpected HTTP Error: API Returned: {httpResponse?.StatusCode} - {ex.Message}", (int)(httpResponse?.StatusCode ?? 0), $"API Error, reason: {ex.Message}", "", listener.GetTime()));
+            }
+            catch (OperationCanceledException ex)
+            {
+                logger.LogCritical(ex, $"Unexpected Timeout Error: {ex.Message}");
+                return Result.Fail(new CustomAPIError($"Unexpected Timeout Error: {ex.Message}", (int)(httpResponse?.StatusCode ?? 0), $"API Error, reason: {ex.Message}", "", listener.GetTime()));
             }
             catch (Exception ex)
             {
