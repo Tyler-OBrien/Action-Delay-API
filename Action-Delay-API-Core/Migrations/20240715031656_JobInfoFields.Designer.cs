48685c53ae5e174dfb9315d49dda9ab4cb55e1ee
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2024-07-15 00:02:13 -0400

    Core: Migration for new Job Fields
    Former-commit-id: b91ac25b733459916881d5ff0d63e3cdb7405b9d

--- Action-Delay-API-Core/Migrations/20240715031656_JobInfoFields.Designer.cs
+++ Action-Delay-API-Core/Migrations/20240715031656_JobInfoFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Action_Delay_API_Core.Models.Database.Postgres;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Action_Delay_API_Core.Migrations
 {
     [DbContext(typeof(ActionDelayDatabaseContext))]
-    partial class ActionDelayDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240715031656_JobInfoFields")]
+    partial class JobInfoFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Action-Delay-API-Core/Migrations/20240715031656_JobInfoFields.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Action_Delay_API_Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class JobInfoFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JobDescription",
+                table: "Jobs",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JobType",
+                table: "Jobs",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Locations",
+                table: "Jobs",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobDescription",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "JobType",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "Locations",
+                table: "Jobs");
+        }
+    }
+}
