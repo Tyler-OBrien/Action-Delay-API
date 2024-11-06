9a86535ca2a1d61441d9447c3a5ff1d7d8f2d899
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2024-11-05 21:43:36 -0500

    API: Job Analytics Config

---
+++ Action-Delay-API-Core/Models/Services/ClickHouse/JobAnalyticsConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_Delay_API_Core.Models.Services.ClickHouse
+{
+    public class JobAnalyticsConfiguration
+    {
+        public string NormalDataSet { get; set; }
+
+        public string NormalDataSetRunStatusFilter { get; set; }
+
+
+        public string ThirtyMinDataSet { get; set; }
+
+        public string TwelthHourDataSet { get; set; }
+
+
+        public Dictionary<JobAnalyticsRequestOptions, string> ColumnNames { get; set; } 
+
+        public Dictionary<JobAnalyticsRequestOptions, string> ColumnNamesAgg { get; set; } 
+    }
+}
