1b4be80586a5ba25194caad27b6f711831a0f287
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2025-03-22 23:15:14 -0400

    Workers, WFP: Add AE Support & x-adp-dur headers

--- scripts/Action-Delay-API-WfP-Worker/src/index.ts
+++ scripts/Action-Delay-API-WfP-Worker/src/index.ts
@@ -1,10 +1,11 @@
 export interface Env {
 	dispatcher: DispatchNamespace 
+	AE: AnalyticsEngineDataset
 }
 export default {
 	async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		var startReq = performance.now();
 		var parseUrl = new URL(req.url);
-
 		try {
 			if (parseUrl.pathname != "/") {
 				return new Response("Nothing here", { status: 404})
@@ -17,24 +18,38 @@ export default {
 			else {
 			 worker = env.dispatcher.get(parseUrl.searchParams.get("scriptName")!);
 			}
-			return await worker.fetch(req, {
-				cf: {
-					image: {
-						width: 90,
-						onerror: "true",
-					}
-				}
-			});	
+			var tryGetWorkerResp =  await worker.fetch(req);	
+			var newHeaders = new Headers(tryGetWorkerResp.headers);
+			newHeaders.set("x-adap-dur", (performance.now() - startReq).toString() )
+			try {
+				if (env.AE)
+					env.AE.writeDataPoint({
+						blobs: [req.cf.colo],
+						doubles: [performance.now() - startReq],
+						indexes: [`wfp`],
+					  });
+			}
+			catch (exception) {
+				console.log(exception)
+			}
+
+			return new Response(tryGetWorkerResp.body, {
+				headers: newHeaders
+			})
 		} catch (e: any) {
 			if (e.message.startsWith('Worker not found')) {
 				// we tried to get a worker that doesn't exist in our dispatch namespace
-				return new Response(e.message, { status: 200 });
+				return new Response(e.message, { status: 200, headers: {
+					'x-adp-dur': (performance.now() - startReq).toString(),
+				} });
 			}
 
 			// this could be any other exception from `fetch()` *or* an exception
 			// thrown by the called worker (e.g. if the dispatched worker has
 			// `throw MyException()`, you could check for that here).
-			return new Response(e.message, { status: 500 });
+			return new Response(e.message, { status: 500, headers: {
+				'x-adp-dur': (performance.now() - startReq).toString(),
+			}  });
 		}
 	},
 };
