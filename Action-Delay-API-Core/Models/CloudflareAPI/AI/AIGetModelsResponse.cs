91e71000b7dc45654eca939b0b026c85e7d40786
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2025-04-14 20:49:33 -0400

    Core: Simply AI Models Endpoint Response Object to fix issues/remove unused properties

--- Action-Delay-API-Core/Models/CloudflareAPI/AI/AIGetModelsResponse.cs
+++ Action-Delay-API-Core/Models/CloudflareAPI/AI/AIGetModelsResponse.cs
@@ -14,33 +14,18 @@ namespace Action_Delay_API_Core.Models.CloudflareAPI.AI
             [JsonPropertyName("id")]
             public string Id { get; set; }
 
-            [JsonPropertyName("source")]
-            public long Source { get; set; }
 
             [JsonPropertyName("name")]
             public string Name { get; set; }
 
-            [JsonPropertyName("description")]
-            public string Description { get; set; }
 
             [JsonPropertyName("task")]
             public Task Task { get; set; }
 
-            [JsonPropertyName("tags")]
-            public string[] Tags { get; set; }
 
-            [JsonPropertyName("properties")]
-            public Property[] Properties { get; set; }
         }
 
-        public partial class Property
-        {
-            [JsonPropertyName("property_id")]
-            public string PropertyId { get; set; }
 
-            [JsonPropertyName("value")]
-            public string Value { get; set; }
-        }
 
         public partial class Task
         {
@@ -50,8 +35,6 @@ namespace Action_Delay_API_Core.Models.CloudflareAPI.AI
             [JsonPropertyName("name")]
             public string Name { get; set; }
 
-            [JsonPropertyName("description")]
-            public string Description { get; set; }
         }
     }
 }
